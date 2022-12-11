296ad902044283c7d5dbe85b53b1a63ddcf51e42
Michael <strafer@strafer.dev>
2022-12-11 22:52:09 +0200

    small improvements to api utils

--- src/app/utils.rs
+++ src/app/utils.rs
@@ -1,14 +1,16 @@
-use std::env;
-
 use reqwest::{
     header::{AUTHORIZATION, CONTENT_TYPE, COOKIE, USER_AGENT},
     RequestBuilder,
 };
 
-pub fn add_headers(request: RequestBuilder, cookie: String, authorization_header: String) -> RequestBuilder {
+pub fn add_headers(
+    request: RequestBuilder,
+    cookie: String,
+    authorization_header: String,
+) -> RequestBuilder {
     return request
     .header(COOKIE, cookie)
-    .header(CONTENT_TYPE, r#"application/json"#)
+    .header(CONTENT_TYPE, "application/json")
     .header("x-xbl-contract-version", "2")
     .header(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36 Like SmartGlass/2.105.0415 CFNetwork/711.3.18 Darwin/14.0.0")
     .header(AUTHORIZATION, authorization_header);
