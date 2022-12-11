e5d327d7436dfcff3d586cdbd0870843d5c3ee64
Michael <strafer@strafer.dev>
2022-12-11 22:11:21 +0200

    finalizing entire flow

--- src/app/auth/flow.rs
+++ src/app/auth/flow.rs
@@ -1,10 +1,16 @@
 use reqwest::Result;
 
-use super::{pre_auth::fetch_pre_auth_data, initial_access_token::fetch_initial_access_token};
+use super::{
+    authenticate::authenticate,
+    authorize::{authorize, AuthorizeResult},
+    initial_access_token::fetch_initial_access_token,
+    pre_auth::fetch_pre_auth_data,
+};
 
-pub async fn fetch_cookies_and_authorization() -> Result<()> {
-    let result = fetch_pre_auth_data().await;
-    let another_result = fetch_initial_access_token(result).await?;
-    println!("{:?}", another_result);
-    Ok(())
+pub async fn fetch_cookies_and_authorization() -> Result<AuthorizeResult> {
+    let pre_auth_payload = fetch_pre_auth_data().await;
+    let initial_access_token_payload = fetch_initial_access_token(pre_auth_payload).await?;
+    let authentication_result = authenticate(initial_access_token_payload).await?;
+    let authorization_result = authorize(authentication_result).await?;
+    Ok(authorization_result)
 }
